134359b06f4fd9c875f6c09608fe460c4ef5ceb5
felipeprestess <felipeadriano_pc@hotmail.com>
2017-03-08 16:24:11 -0300

    #1 Alteração do nome da solução e do projeto

---
+++ GerenciarFluxoDePrograma/Threads/ThreadsFinalizarThreadStatic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+
+    //Podemos marcar um campo com o atributo [ThreadStatic], isso faz com que cada thread
+    //tenha uma cópia do campo, e não um valor global para todas. Se o atributo [ThreadStatic] 
+    //for removido o que for modificado em uma thread será visto pelas outras.
+    public static class ThreadsFinalizarThreadStatic
+    {
+        [ThreadStatic]
+        public static int num;
+        public static void Main()
+        {
+            Console.WriteLine("Thread: {0}",num);
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    num++;
+                    Console.WriteLine("Thread A: {0}", num);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    num++;
+                    Console.WriteLine("Thread B: {0}", num);
+                }
+            }).Start();
+
+            Console.WriteLine("Thread: {0}", num);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GerenciarFluxoDePrograma/Threads/ThreadsUsandoTarefas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+//Com as Tarefas ou Tasks, temos um controle bem melhor das tarefas 
+//que estão sendo executadas, podemos controlar qual tarefa e quando ela irá para 
+//o Pool de Threads, diferente das threads que não temos este controle. 
+//Podemos deixar outras tarefas aguardando enquanto o processador termina uma fila de tarefas,
+//controle que não temos com as threads que vai tudo de uma vez.
+
+//o método T.Wait() é equivalente ao Join da thread que aguarda a finalização da task antes 
+//de continuar o código
+
+namespace Threads
+{
+    class ThreadsUsandoTarefas
+    {
+        public static void Main()
+        {
+            Task T = Task.Run(() => Tarefa());
+            T.Wait();
+            Console.ReadKey();
+        }
+
+         static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa: {0}",i);
+            }
+        }
+    }
+}
