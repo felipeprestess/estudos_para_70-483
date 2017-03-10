3bd95bb7f147c5dc5bba417b36e1d598219a4781
felipeprestess <felipeadriano_pc@hotmail.com>
2017-03-10 11:12:38 -0300

    #1 Desenvolvimento de Tasks

---
+++ GerenciarFluxoDePrograma/GereciamentoDeFluxoDePrograma/ContinuandoTarefaDeMultiplas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+//Também pode-se optar que somente uma das Tasks finalizem para dar continuidade a execução,
+//neste caso é só trocar o WaitAll por WaitAny
+
+namespace GereciamentoDeFluxoDePrograma
+{
+    class ContinuandoTarefaDeMultiplas
+    {
+        public static void Main()
+        {
+
+            Task<int>[] tasks = new Task<int>[3];
+
+            tasks[0] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 1;
+            });
+
+            tasks[1] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 2;
+            });
+
+            tasks[2] = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return 3;
+            });
+
+            while (tasks.Length > 0)
+            {
+                int i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GerenciarFluxoDePrograma/GereciamentoDeFluxoDePrograma/TarefasEmEstado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+//O método ContinueWith() também pode receber parâmetros que irão indicar em que situações
+//ele irá continuar, como por exemplo se tiver um erro, ou se for processado com sucesso
+
+namespace GereciamentoDeFluxoDePrograma
+{
+    class TarefasEmEstado
+    {
+        public static void Main(string[] args)
+        {
+            Task<int> T = Task.Run(() => 
+            {
+                return 10;
+            });
+
+            T.ContinueWith((i) =>
+            {
+                Console.WriteLine("Cancelado");
+            },TaskContinuationOptions.OnlyOnCanceled);
+
+            T.ContinueWith((i) =>
+            {
+                Console.WriteLine("Erro");
+            },TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = T.ContinueWith((i) =>
+            {
+                Console.WriteLine("Finalizado");
+            },TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+            Console.ReadKey();
+        }
+    }
+}
